7695122a1121e1a43f1fea3b4397840b57a7d249
vctriusss <vskamaev@miem.hse.ru>
2024-03-10 16:48:43 +0300

    feat: added simple version

--- cmd/vernam/vernam.go
+++ cmd/vernam/vernam.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"crypto/rand"
 	"errors"
 	"log"
 
 	"github.com/urfave/cli/v2"
-	"github.com/vctriusss/vernam-cipher/internal/alphabet"
 	"github.com/vctriusss/vernam-cipher/internal/files"
 
 	"os"
@@ -23,50 +23,7 @@ var (
 		&cli.StringFlag{Name: "output", Aliases: []string{"o"}, Usage: "Name of output `FILE`", Required: true},
 		&cli.StringFlag{Name: "key", Aliases: []string{"k"}, Usage: "Name of key `FILE`", Required: true},
 	}
-
-	alphabetFlags = []cli.Flag{
-		// language
-		&cli.BoolFlag{Name: "eng", Usage: "English alphabet", Category: "Language", DisableDefaultText: true},
-		&cli.BoolFlag{Name: "rus", Usage: "Russian alphabet", Category: "Language", DisableDefaultText: true},
-		// mode flags
-		&cli.BoolFlag{Name: "lower", Aliases: []string{"l"}, Usage: "Include lowercase letters", Category: "Alphabet extensions", DisableDefaultText: true},
-		&cli.BoolFlag{Name: "upper", Aliases: []string{"u"}, Usage: "Include uppercase letters", Category: "Alphabet extensions", DisableDefaultText: true},
-		&cli.BoolFlag{Name: "digits", Aliases: []string{"d"}, Usage: "Include digits", Category: "Alphabet extensions", DisableDefaultText: true},
-		&cli.BoolFlag{Name: "punctuation", Aliases: []string{"p"}, Usage: "Include punctuation signs", Category: "Alphabet extensions", DisableDefaultText: true},
-		&cli.BoolFlag{Name: "space", Aliases: []string{"s"}, Usage: "Include space", Category: "Alphabet extensions", DisableDefaultText: true},
-	}
 )
-
-func alphabetFromCtx(ctx *cli.Context) (*alphabet.Alphabet, error) {
-	sets := make([][]rune, 0)
-
-	if ctx.Bool("eng") && ctx.Bool("lower") {
-		sets = append(sets, alphabet.EnglishLowerCase)
-	}
-	if ctx.Bool("eng") && ctx.Bool("upper") {
-		sets = append(sets, alphabet.EnglishUpperCase)
-	}
-	if ctx.Bool("rus") && ctx.Bool("lower") {
-		sets = append(sets, alphabet.RussianLowerCase)
-	}
-	if ctx.Bool("rus") && ctx.Bool("upper") {
-		sets = append(sets, alphabet.RussianUpperCase)
-	}
-	if ctx.Bool("space") {
-		sets = append(sets, []rune{' '})
-	}
-	if ctx.Bool("digits") {
-		sets = append(sets, alphabet.Digits)
-	}
-	if ctx.Bool("punctuation") {
-		sets = append(sets, alphabet.Signs)
-	}
-	if len(sets) == 0 {
-		return nil, errors.New("couldn't create alphabet from flags")
-	}
-
-	return alphabet.New(alphabet.ComposeAlphabet(sets...)), nil
-}
 
 func main() {
 	app := &cli.App{
@@ -77,72 +34,71 @@ func main() {
 			{
 				Name:  "encrypt",
 				Usage: "Encrypt a file",
-				Flags: append(encFlags, alphabetFlags...),
+				Flags: encFlags,
 				Action: func(ctx *cli.Context) error {
-					a, err := alphabetFromCtx(ctx)
-					if err != nil {
-						return err
-					}
-
 					inputBytes, err := files.ReadInput(ctx.String("input"))
 					if err != nil {
 						return err
 					}
 
-					inputText := string(inputBytes)
-
-					var key string
+					var key []byte
 
 					if ctx.String("key") == "" {
-						key = a.RandKey(len([]rune(inputText)))
-						if err := files.WriteOutput("key.txt", []byte(key)); err != nil {
-							return err
-						}
-					} else {
-						keyBytes, err := files.ReadInput(ctx.String("key"))
+						key = make([]byte, len(inputBytes))
+						_, err := rand.Read(key)
 						if err != nil {
 							return err
 						}
-						key = string(keyBytes)
+
+						if err := files.WriteOutput("key.txt", key); err != nil {
+							return err
+						}
+					} else {
+						key, err = files.ReadInput(ctx.String("key"))
+						if err != nil {
+							return err
+						}
 					}
 
-					encrypted, err := a.Encrypt(inputText, key)
-					if err != nil {
-						return err
+					if len(key) != len(inputBytes) {
+						return errors.New("text and key must have the same length")
 					}
 
-					return files.WriteOutput(ctx.String("output"), []byte(encrypted))
+					encrypted := make([]byte, len(inputBytes))
+
+					for i := range encrypted {
+						encrypted[i] = inputBytes[i] ^ key[i]
+					}
+
+					return files.WriteOutput(ctx.String("output"), encrypted)
 				},
 			},
 			{
 				Name:  "decrypt",
 				Usage: "Decrypt a file",
-				Flags: append(decFlags, alphabetFlags...),
+				Flags: decFlags,
 				Action: func(ctx *cli.Context) error {
-					a, err := alphabetFromCtx(ctx)
-					if err != nil {
-						return err
-					}
-
 					inputBytes, err := files.ReadInput(ctx.String("input"))
 					if err != nil {
 						return err
 					}
 
-					inputText := string(inputBytes)
-
-					keyBytes, err := files.ReadInput(ctx.String("key"))
-					if err != nil {
-						return err
-					}
-					key := string(keyBytes)
-
-					decrypted, err := a.Decrypt(inputText, key)
+					key, err := files.ReadInput(ctx.String("key"))
 					if err != nil {
 						return err
 					}
 
-					return files.WriteOutput(ctx.String("output"), []byte(decrypted))
+					if len(key) != len(inputBytes) {
+						return errors.New("text and key must have the same length")
+					}
+
+					decrypted := make([]byte, len(inputBytes))
+
+					for i := range decrypted {
+						decrypted[i] = inputBytes[i] ^ key[i]
+					}
+
+					return files.WriteOutput(ctx.String("output"), decrypted)
 				},
 			},
 		},
